daa8cb80a46095977a526003c7b0afe02f9cc3fd
galuf <alex.flores.m@uni.pe>
2019-05-11 23:54:28 -0500

    Dia 1

---
+++ first-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './componentes/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />  
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
