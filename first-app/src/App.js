c387a977b5724721368b0a59dc936de6ff76c12c
Louiso <Gransullca.25@gmail.com>
2019-05-12 20:07:57 -0500

    cambie algo

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './componentes/Navigation';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./componentes/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />  
+      <Navigation /> awawdasdawda
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
